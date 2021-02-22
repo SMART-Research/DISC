f22046cfce6c75c5bd156bedd45faad271e217b7
thiyangt <thiyanga.talagala@monash.edu>
2021-02-22 12:39:47 +0530

    format the dataset

---
+++ R/disc.R
@@ -0,0 +1,17 @@
+#'Weekly notified diseases reported by Ministry of Health, Nutrition
+#'and Indigenous Medicine in Sri Lanka
+#'
+#'\describe{
+#'\item{ID}{row id}
+#'\item{Year}{Year}
+#'\item{Week}{Week number to locate the week within a year}
+#'\item{StartData}{Starting date of the week}
+#'\item{EndData}{End date of the week}
+#'@source Weekly epidemiological reports published by the Epidemiological Unit, Ministry of Health, Sri Lanka
+#'}
+#'
+#'@examples
+#'data(Data_Counts)
+#'head(Data_Counts)
+#'summary(Data_Counts)
+"Data_Counts"

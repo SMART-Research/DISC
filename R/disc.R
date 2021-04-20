5d171d03e240c247a684c9a6ee734d210b127acc
SamudraMadushani <samudramadushani@gmail.com>
2021-04-20 01:20:17 -0700

    Add remaining descriptions

--- R/disc.R
+++ R/disc.R
@@ -9,6 +9,19 @@
 #'\item{EndData}{End date of the week}
 #'@source Weekly epidemiological reports published by the Epidemiological Unit, Ministry of Health, Sri Lanka
 #'}
+#'\item{Division}{Districts of Sri Lanka}
+#'\item{Dengue}{Weekly Dengue counts}
+#'\item{Dysentery}{Weekly Dysentery counts}
+#'\item{Encephalitis}{Weekly Encephalitis counts}
+#'\item{Enteric Fever}{Weekly Enteric Fever Counts}
+#'\item{Food Poisioning}{Weekly Food Poisioning Counts}
+#'\item{Leptospirosis}{Weekly Leptospirosis Counts}
+#'\item{Typhus Fever}{Weekly Typhus Fever Counts}
+#'\item{Viral Hepatistis}{Weekly Viral Hepatistis Counts}
+#'\item{Human Rabies}{Weekly Human Rabies Counts}
+#'\item{Chickenpox}{Weekly Chickenpox Counts}
+#'\item{Meningitis}{Weekly Meningitis Counts}
+#'\item{Leishmaniasis}{Weekly Leishmaniasis Counts}
 #'
 #'@examples
 #'data(Data_Counts)
